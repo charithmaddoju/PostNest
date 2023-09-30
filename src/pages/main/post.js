d514821911e438f77ef31b7fe79b3316894e7498
Charith <charithmaddoju27@gmail.com>
2023-09-30 20:37:55 +0530

    Add likes collection

--- src/pages/main/post.js
+++ src/pages/main/post.js
@@ -1,21 +1,32 @@
 import '../../styles/Main.css'
-import { useState } from 'react';
-import { addDoc, collection} from "firebase/firestore";
+import { useEffect, useState } from 'react';
+import { addDoc, getDocs, collection, query, where} from "firebase/firestore";
 import { auth, db } from "../../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 
 export const Post = (props) => {
-
-    const likesRef = collection(db, "likes");
+    const [likeAmount,setLikeAmount] = useState(null);
+    
     const [user] = useAuthState(auth);
 
+    const likesRef = collection(db, "likes");
+    const likesDoc = query(likesRef, where("postId", "==", props.id));
+    const getLikes = async() => {
+        const data = await getDocs(likesDoc)
+        setLikeAmount(data.docs.length);
+    }
+    
     const addLike = async () => {
         await addDoc(likesRef, {
             postId : props.id,
             userId : user.uid
         })
     }
+
+    useEffect(() => {
+        getLikes();
+    },[])
 
     return(
         <div className="Post">
@@ -24,7 +35,7 @@ export const Post = (props) => {
             <b>@{props.username}</b>
             <div>
                 <button onClick={addLike}> &#128077;</button>
-                <button > &#128078;</button>
+                {likeAmount && <p>Likes : {likeAmount}</p>}
             </div>
         </div>
    )
