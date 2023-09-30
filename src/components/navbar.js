2041a3cf7911e175ed247631f87a262192081ac9
Charith <charithmaddoju27@gmail.com>
2023-09-30 16:05:43 +0530

    Add basic styles

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom"
 import {auth} from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-
+import '../styles/navbar.css'
 export const Navbar = () => {
 
     const [user] = useAuthState(auth);
 
     return (
-        <nav>
-            <Link to='/'>Home </Link>
-            <Link to='/login'>Login</Link>
-            <p>{user?.displayName}</p>
-            <img src={user?.photoURL || "" } alt="user" />
+        <nav className="Navbar">
+            <div className="Title">PostNest</div>
+            <ul>
+                <li>
+                    <Link to='/'>Home </Link>
+                    <Link to='/login'>Login</Link>
+                </li>
+            </ul>
+            <div className="User">
+                <p className="User-name">{user?.displayName}</p>
+                <img className="User-img" src={user?.photoURL || "" } alt="user" />
+            </div>
         </nav>
     )
 }
